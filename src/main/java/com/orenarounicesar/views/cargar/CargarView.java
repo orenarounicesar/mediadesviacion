309bdbfeb80faa6f375df551d93bb003caf73ff2
start.vaadin.com <no-reply@vaadin.com>
2022-05-10 00:58:46 +0000

    Generated project

---
+++ src/main/java/com/orenarounicesar/views/cargar/CargarView.java
@@ -0,0 +1,33 @@
+package com.orenarounicesar.views.cargar;
+
+import com.orenarounicesar.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Cargar")
+@Route(value = "cargar", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class CargarView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public CargarView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
